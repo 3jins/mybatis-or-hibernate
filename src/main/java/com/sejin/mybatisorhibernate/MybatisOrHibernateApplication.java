8df57d8b5c997e098aba6d40ad6f5e0c1f59512b
Sejin Jeon <jinaidy93@gmail.com>
2019-08-14 18:41:22 +0900

    Generate basic Spring code

---
+++ src/main/java/com/sejin/mybatisorhibernate/MybatisOrHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.sejin.mybatisorhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisOrHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisOrHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sejin/mybatisorhibernate/MybatisOrHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sejin.mybatisorhibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisOrHibernateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
